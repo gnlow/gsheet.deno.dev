41ab41f8d47a1cb667f35c5b1eb35eee1b6bfa56
Gnlow <gnlow@protonmail.com>
2024-02-11 17:41:10 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
-import { parse } from "https://deno.land/std@0.215.0/csv/mod.ts"
+const googleSheet = (id: string) => fetch(
+    `https://docs.google.com/spreadsheets/u/0/d/${id}/export?format=csv`
+).then(x => x.text())
 
-const googleSheet = async (id: string) => {
-    const data = await fetch(`https://docs.google.com/spreadsheets/u/0/d/${id}/export?format=csv`).then(x => x.text())
-
-    return parse(data)
-}
-
-Deno.serve((req: Request) => {
+Deno.serve(async (req: Request) => {
     const url = new URL(req.url)
-    return new Response(url.href, {
-        headers: new Headers({
-            "content-type": "text/csv",
-            "access-control-allow-origin": "*",
-        }),
-    })
+    const id = url.pathname.substring(1)
+
+    return new Response(
+        await googleSheet(id), 
+        {
+            headers: new Headers({
+                "content-type": "text/csv",
+                "access-control-allow-origin": "*",
+            }),
+        }
+    )
 })
